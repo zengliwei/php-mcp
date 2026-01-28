4505f9c6c3627cbdc41972ba3cf885e7d578f2db
zengliwei <zengliwei@163.com>
2026-01-28 21:34:22 +0800

    update source files

--- public/index.php
+++ public/index.php
@@ -4,21 +4,26 @@ define('ROOT', dirname(__DIR__));
 
 require_once ROOT . '/vendor/autoload.php';
 
+use GuzzleHttp\Psr7\Response;
 use Http\Discovery\Psr17Factory;
 use Lib\Logger;
 use Mcp\Server;
+use Mcp\Server\Session\FileSessionStore;
 use Mcp\Server\Transport\StreamableHttpTransport;
 
-header('Content-Type: application/json');
-
 /**
+ * @var Response $response
  * @see https://github.com/modelcontextprotocol/php-sdk
  */
-echo Server::builder()
+$response = Server::builder()
     ->setServerInfo('MCP Server', '1.0.0')
     ->setLogger(new Logger('server.log'))
     ->setDiscovery(ROOT, ['mcp-server'])
+    ->setSession(new FileSessionStore(ROOT . '/var/sessions'))
     ->build()
-    ->run(new StreamableHttpTransport((new Psr17Factory())->createServerRequestFromGlobals()))
-    ->getBody()
-    ->getContents();
+    ->run(new StreamableHttpTransport((new Psr17Factory())->createServerRequestFromGlobals()));
+
+foreach ($response->getHeaders() as $k => $v) {
+    header("$k: {$v[0]}");
+}
+echo $response->getBody()->getContents();
