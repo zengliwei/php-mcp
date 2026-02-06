f40c5c16b2a6225c446f40a775b47c7fb2c4450f
zengliwei <zengliwei@163.com>
2026-02-06 10:33:10 +0800

    update source files

--- client-http.php
+++ client-http.php
@@ -8,10 +8,9 @@ use PhpMcp\Client\Enum\TransportType;
 use PhpMcp\Client\Model\Capabilities;
 use PhpMcp\Client\ServerConfig;
 
-header('Content-Type: application/json');
-
 $server = filter_input_array(INPUT_SERVER);
 if (!preg_match('/^\/client\/(?P<path>[\da-z\-]+)$/', $server['REQUEST_URI'], $matches)) {
+    header('Content-Type: application/json');
     echo json_encode(['error' => 'Invalid request']);
     exit;
 }
@@ -32,9 +31,12 @@ $client = Client::make()
 
 try {
     $client->initialize();
-    echo match ($matches['path']) {
-        'list-tools' => json_encode($client->listTools())
+    $result = match ($matches['path']) {
+        'list-tools' => $client->listTools()
     };
 } catch (Throwable $e) {
-    echo json_encode(['error' => $e->getMessage()]);
+    $result = ['error' => $e->getMessage(), 'trace' => $e->getTraceAsString()];
+} finally {
+    header('Content-Type: application/json');
+    echo json_encode($result);
 }
